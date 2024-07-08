2b637c2188eeec93fcc76b0ed9cba06673f69df0
ericv2024 <v.ericmarinescu@gmail.com>
2024-07-08 12:58:54 +0000

    wip

--- scripsts/main.js
+++ scripsts/main.js
@@ -1,10 +1,12 @@
 import images from "./images.js";
 const poze = document.querySelector(".poze");
 const scor = document.querySelector(".scor");
+const pesteButton = document.querySelector(".peste");
+const nonPesteButton = document.querySelector(".nonPeste");
 
 let pozeAfisate = 3;
 
-scor.innerHTML = `scorul tau este: 0 / ${images.length}`
+scor.innerHTML = `scorul tau este: 0 / ${images.length}`;
 
 const pozeleMele = images.slice(images.length - pozeAfisate);
 
@@ -16,3 +18,11 @@ poze.innerHTML = pozeleMele
       }">`
   )
   .join();
+
+pesteButton.addEventListener("click", () => {
+  console.log("peste");
+});
+
+nonPesteButton.addEventListener("click", () => {
+  console.log("nu e peste");
+});
